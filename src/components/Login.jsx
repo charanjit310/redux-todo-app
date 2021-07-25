6ab344d90db88582e8830751bf02564d310c005a
charanjit <charanjitsingh@zapbuild.com>
2021-07-25 13:35:50 +0530

    login form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 import "../assets/Register.css";
 import { Link } from 'react-router-dom';
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  email: yup.string().email('Email must be valid').required('Email is required'),
+  password: yup.string().required('Password is required').min(6).max(8),
+});
+
 function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const submitForm = (data) => {
+    console.log(data);
+  }
   return (
     <div>
       <div className="container login-container">
@@ -13,14 +28,17 @@ function Login() {
               <span>Don't have an account ? </span>
               <Link className="linkbtn" to="/register">Sign up</Link>
             </div>
-            <form>
+            <form onSubmit={handleSubmit(submitForm)}>
               <div className="form-group mb-3">
-                <input type="text" name="email" className="form-control" placeholder="Your Email *" value="" />
+                <input type="text" {...register('email')} className="form-control" placeholder="Your Email *" />
               </div>
+              <span className="red">{errors.email && errors.email.message}</span>
 
               <div className="form-group mb-3">
-                <input type="password" name="password" className="form-control" placeholder="Your Password *" value="" />
+                <input type="password" {...register('password')} className="form-control" placeholder="Your Password *" />
               </div>
+              <span className="red">{errors.password && errors.password.message}</span>
+
               <div className="form-group">
                 <input type="submit" className="btnSubmit" value="Login" />
               </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import "../assets/Register.css";
+
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
