50e8acd7fc67c99988c999dce5d3d6402508d727
charanjit <charanjitsingh@zapbuild.com>
2021-08-25 23:31:58 +0530

    pagination

--- src/Services/dashboard.service.js
+++ src/Services/dashboard.service.js
@@ -1,7 +1,7 @@
 import axios from '../Services/axiosInstance';
 
 const list = (params = {}) => {
-  return axios.get(`/users-listing?role=${params.role}`)
+  return axios.get(`/users-listing?role=${params.role}&&page=${params.page}`)
 }
 
 export const DashboardServices = {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import "../assets/CustomTables.css";
 import { useSelector, useDispatch } from 'react-redux'
 import { DashboardServices } from '../Services/dashboard.service';
+import Pagination from 'react-js-pagination';
 
 function Home() {
   const dispatch = useDispatch();
   const [userList, setUserList] = useState([])
   const [loading, setLoading] = useState(true)
 
+  const [currentPage, setCurrentPage] = useState(1)
+  const [paginationData, setpaginationData] = useState([])
+
   useEffect(() => {
     let isSubscribed = true
-    DashboardServices.list({ 'role': 'clinic' }).then((res) => {
+    DashboardServices.list({ 'role': 'clinic', 'page': currentPage }).then((res) => {
       if (isSubscribed) {
         console.log(res.data);
         setUserList(res.data.data.data)
+        setpaginationData(res.data.data)
         setLoading(false)
       }
     }).catch((error) => {
@@ -24,8 +29,61 @@ function Home() {
 
     return () => isSubscribed = false // cleanup function 
 
-  }, []);
+  }, [currentPage]);
 
+  const handleEdit = (id) => {
+    alert(id)
+  }
+
+  const handleDelete = (id) => {
+    alert(id)
+  }
+
+  const renderList = (list) => {
+    return (
+      list.map((list) => {
+        return (
+          <tr key={list.id}>
+            <td>{list.id}</td>
+            <td>{list.name}</td>
+            <td>{list.email}</td>
+            <td>{list.phone}</td>
+            <td>
+              <button onClick={() => handleEdit(list.id)} className="btn btn-primary" >Edit</button>
+              <button onClick={() => handleDelete(list.id)} className="btn btn-danger" style={{ marginLeft: 10 }}>Delete</button>
+            </td>
+          </tr>
+        )
+      })
+    )
+  }
+
+  const renderPaginatioin = ({ current_page, total, per_page }) => {
+    return (
+      <div className="col-12 mt-4 text-end">
+        <Pagination
+          activePage={current_page}
+          totalItemsCount={total}
+          itemsCountPerPage={per_page}
+          itemClass='page-item'
+          linkClass='page-link'
+          firstPageText='First'
+          lastPageText='Last'
+          onChange={(pageNumber) => {
+            setCurrentPage(pageNumber)
+          }}
+        />
+      </div>
+    )
+  }
+
+  // // Logic for displaying todos
+  // const indexOfLastTodo = currentPage * perPage;
+  // const indexOfFirstTodo = indexOfLastTodo - perPage;
+  // const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+  // // https://academind.com/tutorials/reactjs-pagination/
+  // // https://hemanta.io/implement-pagination-search-and-filter-in-a-react-app-part-3/
+  // // https://stackoverflow.com/questions/40232847/how-to-implement-pagination-in-react
   return (
     <div>
       <div className="container">
@@ -46,27 +104,12 @@ function Home() {
               </thead>
               <tbody>
                 {loading && <tr><td colSpan="5" className="text-center"><span className="spinner-grow text-primary " style={{ marginRight: '11px' }}> </span></td></tr>}
-                {
-                  userList.map((list) => {
-                    return <tr key={list.id}>
-                      <td>{list.id}</td>
-                      <td>{list.name}</td>
-                      <td>{list.email}</td>
-                      <td>{list.phone}</td>
-                      <td>
-                        <button className="btn btn-primary" >Edit</button>
-                        <button className="btn btn-danger" style={{ marginLeft: 10 }}>Delete</button>
-                      </td>
-                    </tr>
-                  })
-                }
+                {!loading && renderList(userList)}
               </tbody>
             </table>
+            {!loading && renderPaginatioin(paginationData)}
           </div>
         </div>
-        {/* <div className="row" style={{ background: 'grey' }}>
-          <b>TWO</b>
-        </div> */}
       </div>
     </div >
   )
