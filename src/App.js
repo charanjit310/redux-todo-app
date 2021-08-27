717f68f2a385d0f23ff7695b8a14436628a0af3b
charanjit <charanjitsingh@zapbuild.com>
2021-08-27 17:19:26 +0530

    async await

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ import VerifyOTP from "./components/VerifyOTP";
 import { useSelector, useDispatch } from 'react-redux'
 import { AuthsService } from "./Services/auth.service";
 import { PrivateRoute, GuestRoute } from "./midleweres";
-
+import axios from "axios";
 
 function App() {
   const isLoggedIn = useSelector((state) => {
     return state.registerReducer.loggedIn
   })
-  console.log(isLoggedIn);
+  // console.log(isLoggedIn);
   // let p = new Promise((res, rej) => {
   //   let i = 3
   //   if (i == 2) {
@@ -36,10 +36,23 @@ function App() {
 
   // async function harry() { // async functions always returns Promise
   //   console.log('before fetch');
-  //   let res = await fetch('https://api.github.com/users)
-  //   res = await res.json();
-  //   console.log('after fetch');
-  //   return res;
+  //   // let res = await fetch('https://api.github.com/users')
+  //   // res = await res.json(); // await res.json() is a method on the Response object that lets you extract a JSON object from the response. The method returns a promise, so you have to wait for the JSON: await response.json().
+  //   // console.log('after first fetch');
+  //   // let res1 = await fetch(`https://api.github.com/user/${res[0].id}`)
+  //   // res1 = await res1.json();
+  //   // console.log('after second fetch');
+  //   // return res;
+
+  //   // using axios 
+  //   // let res = await axios.get('https://api.github.com/users')
+  //   // console.log('after first fetch');
+  //   // console.log(res.data);
+  //   // let res1 = await axios.get(`https://api.github.com/user/${res.data[0].id}`)
+  //   // console.log('after second fetch');
+  //   // console.log(res1.data);
+  //   // return res;
+
   // }
 
   // const a = harry();
