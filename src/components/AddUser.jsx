72ba85d178d9961e12291f446b69ae2c1b1adc96
charanjit <charanjitsingh@zapbuild.com>
2021-09-15 23:16:02 +0530

    dynamic json form

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -2,19 +2,53 @@ import React from 'react'
 import Forms from './Resuable/Forms'
 
 function AddUser() {
+  const template = {
+    title: "Add User",
+    fields: [
+      {
+        title: 'name',
+        type: "text",
+        fieldName: "name"
+      },
+      {
+        title: 'email',
+        type: "email",
+        fieldName: "email"
+      },
+      {
+        title: 'password',
+        type: "password",
+        fieldName: "password"
+      },
+      {
+        title: 'password_confirmation',
+        type: "password",
+        fieldName: "password_confirmation"
+      },
+      {
+        title: 'user_type',
+        type: "select",
+        fieldName: "user_type"
+      },
+    ]
+  }
   return (
     <div>
       <div className="container">
-        <div className="row">
+        <div className="row justify-content-center">
           <div>
             <div className="col-md-4 py-4 float-start">
             </div>
           </div>
-          <Forms />
+          <Forms template={template} submitForm={submitForm} />
         </div>
-      </div>
+      </div >
     </div >
   )
 }
 
+const submitForm = (data) => {
+  console.log('formdataaaa');
+  console.log(data);
+}
 export default AddUser
--- src/components/Resuable/Forms.jsx
+++ src/components/Resuable/Forms.jsx
@@ -1,11 +1,117 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { formValidations } from './FormsValidations';
+const schema = yup.object().shape(formValidations.userFormValidate);
 
-function Forms() {
+// Resuable Form Component
+function Forms({ template, submitForm }) {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const { title, fields, } = template;
+
   return (
-    <div>
-      <h2>Forms</h2>
+    <div className="col-md-8 login-form-1 ">
+      <h3 className="float-start">{title} </h3>
+      <form onSubmit={handleSubmit(submitForm)}>
+        {
+          fields.map((field) => {
+            const { title, type, fieldName } = field;
+
+            switch (type) {
+              case "text":
+                return (
+                  <div className="form-group mb-3" key={fieldName}>
+                    <input type={type} {...register(fieldName)} className="form-control" placeholder={`Your ${fieldName} *`} />
+                    <span className="red">{errors[fieldName] && errors[fieldName].message}</span>
+                  </div>
+                )
+              case "email":
+                return (
+                  <div className="form-group mb-3" key={fieldName}>
+                    <input type={type} {...register(fieldName)} className="form-control" placeholder={`Your ${fieldName} *`} />
+                    <span className="red">{errors[fieldName] && errors[fieldName].message}</span>
+                  </div>
+                )
+              case "password":
+                return (
+                  <div className="form-group mb-3" key={fieldName}>
+                    <input type={type} {...register(fieldName)} className="form-control" placeholder={`Your ${fieldName} *`} />
+                    <span className="red">{errors[fieldName] && errors[fieldName].message}</span>
+                  </div>
+                )
+              case "select":
+                return (
+                  <div className="form-group mb-3" key={fieldName}>
+                    <select className="form-control" {...register('user_type')} >
+                      <option value="">Select Profile</option>
+                      <option value="Doctor">Doctor</option>
+                      <option value="Patient">Patient</option>
+                    </select>
+                    <span className="red">{errors.user_type && errors.user_type.message}</span>
+                  </div>
+                )
+              default:
+                return (
+                  <div className="form-group mb-3" key={fieldName}>
+                    <span className="red">Invalid Field Added</span>
+                  </div>
+                )
+            }
+          })
+        }
+
+        <button className="btn btn-primary btnSubmit float-end" >Add</button>
+        {/* {console.log(errors)} */}
+
+
+
+
+
+
+
+
+        {/* <div className="form-group mb-3">
+          <input type="text" {...register('name')} className="form-control" placeholder="Your Name *" />
+          <span className="red">{errors.name && errors.name.message}</span>
+        </div>
+
+        <div className="form-group mb-3">
+          <input type="text" {...register('email')} className="form-control" placeholder="Your Email *" />
+          <span className="red">{errors.email && errors.email.message}</span>
+        </div>
+
+        <div className="form-group mb-3">
+          <input type="password" {...register('password')} className="form-control" placeholder="Your Password *" />
+          <span className="red">{errors.password && errors.password.message}</span>
+        </div>
+
+        <div className="form-group mb-3">
+          <input type="password" {...register('password_confirmation')} className="form-control" placeholder="Your Confirm Password *" />
+          <span className="red">{errors.password_confirmation && errors.password_confirmation.message}</span>
+        </div>
+
+        <div className="form-group mb-3">
+          <select className="form-control" {...register('user_type')}>
+            <option value="">Select Profile</option>
+            <option value="Doctor">Doctor</option>
+            <option value="Patient">Patient</option>
+          </select>
+          <span className="red">{errors.user_type && errors.user_type.message}</span>
+        </div>
+
+        <button className="btn btn-primary btnSubmit" >
+          Add
+        </button> */}
+
+      </form>
     </div>
   )
 }
+
+
 
 export default Forms
